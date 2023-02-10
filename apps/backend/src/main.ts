8ae412abff1e17a6170050286b37feed4be66f18
ETRI 박성훈 <shonn.dev@gmail.com>
2023-02-10 15:51:13 +0900

    chore: validate setting & extends option remove

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,5 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(8000); // TODO 환경변수로 관리
+
 }
 bootstrap();
