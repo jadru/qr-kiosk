b4cfc4340435533308177ceb75ac7fe38ea3e80d
Younggun Park <ygpark0922@gmail.com>
2023-02-22 18:14:13 +0900

    feat: bottom nav. design change

--- apps/frontend/src/components/GlobalLayout/NavBar.tsx
+++ apps/frontend/src/components/GlobalLayout/NavBar.tsx
@@ -1,6 +1,6 @@
 export const NavBar = () => (
-  <div className="btm-nav w-full">
-    <button className="text-primary">
+  <div className="btm-nav">
+    <button>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         className="h-5 w-5"
@@ -11,12 +11,13 @@ export const NavBar = () => (
         <path
           strokeLinecap="round"
           strokeLinejoin="round"
-          stroke-width="2"
+          strokeWidth="2"
           d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
         />
       </svg>
+      <span className="btm-nav-label">Home</span>
     </button>
-    <button className="text-primary active">
+    <button className="active">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         className="h-5 w-5"
@@ -27,12 +28,13 @@ export const NavBar = () => (
         <path
           strokeLinecap="round"
           strokeLinejoin="round"
-          stroke-width="2"
+          strokeWidth="2"
           d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
         />
       </svg>
+      <span className="btm-nav-label">Warnings</span>
     </button>
-    <button className="text-primary">
+    <button>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         className="h-5 w-5"
@@ -43,10 +45,11 @@ export const NavBar = () => (
         <path
           strokeLinecap="round"
           strokeLinejoin="round"
-          stroke-width="2"
+          strokeWidth="2"
           d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
         />
       </svg>
+      <span className="btm-nav-label">Statics</span>
     </button>
   </div>
 );
