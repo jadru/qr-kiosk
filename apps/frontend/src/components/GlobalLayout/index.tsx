2c5e75ce5b085f0f3edd9a187fbf1a717a911083
Younggun Park <ygpark0922@gmail.com>
2023-02-25 21:13:42 +0900

    feat: add qr setup

--- apps/frontend/src/components/GlobalLayout/index.tsx
+++ apps/frontend/src/components/GlobalLayout/index.tsx
@@ -1,4 +1,5 @@
 import { tabAtom } from '@src/states/atom';
+import QRCode from 'react-qr-code';
 import { useRecoilValue } from 'recoil';
 import { NavBar } from './NavBar';
 export const GlobalLayout = () => {
@@ -7,6 +8,12 @@ export const GlobalLayout = () => {
     <div className="w-full h-full p-6">
       <p className="text-3xl">Hello world!</p>
       <p className="text-2xl">Selected tab is {tab}</p>
+      <QRCode
+        size={256}
+        style={{ height: 'auto', maxWidth: '100%', width: '50%' }}
+        value={'http://localhost:3000'}
+        viewBox={`0 0 256 256`}
+      />
       <NavBar />
     </div>
   );
