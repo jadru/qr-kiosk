3ee63547f8d5a5471998f0dd5e611faefa0c1501
Younggun Park <ygpark0922@gmail.com>
2023-03-03 13:51:38 +0900

    feat: main page design change

--- apps/frontend/src/components/NormalLayout/index.tsx
+++ apps/frontend/src/components/NormalLayout/index.tsx
@@ -3,7 +3,7 @@ import { NavBar } from './NavBar';
 
 export const NormalLayout = ({ children }: ContentType) => {
   return (
-    <div className="w-full h-full p-4">
+    <div className="w-full h-full">
       <NavBar />
       <div className="px-8 py-6">{children}</div>
     </div>
