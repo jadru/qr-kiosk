3b2395262b24a76077747d22bf9b24f83503705e
Younggun Park <ygpark0922@gmail.com>
2023-03-03 13:39:17 +0900

    feat: change error page design

--- apps/frontend/src/components/ErrorScreen/index.tsx
+++ apps/frontend/src/components/ErrorScreen/index.tsx
@@ -1,11 +1,12 @@
 import Lottie from 'react-lottie';
+import { Link } from 'react-router-dom';
 import { NormalLayout } from '../NormalLayout';
 import ErrorLottie from './98488-bot-error-404.json';
 
 export const ErrorScreen = () => {
   return (
     <NormalLayout>
-      <div className="w-full h-full flex justify-center">
+      <div className="w-full h-full justify-center self-center text-center space-y-4">
         <Lottie
           options={{
             loop: true,
@@ -16,7 +17,16 @@ export const ErrorScreen = () => {
             },
           }}
           isClickToPauseDisabled
+          width="80%"
+          height="80%"
         />
+        <p className="text-2xl font-normal text-center">
+          페이지를 찾을 수 없습니다.
+        </p>
+
+        <button className="btn">
+          <Link to="/">메인으로 가기</Link>
+        </button>
       </div>
     </NormalLayout>
   );
