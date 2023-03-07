99cf62ce14c8dfca59066728d659af9f1b52d121
MoonSangWon <kuim75@naver.com>
2023-03-07 14:06:07 +0900

    feat: Prisma service, module setting && Owner, Menu, Menu_Item entity create

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,12 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, Controller } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-    imports: [ConfigModule.forRoot({
-        isGlobal: true
-    })],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
