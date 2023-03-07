04c7d5185c82e32c58f0dd99a620f5235bfc13ea
Hoon9901 <shonn.dev@gmail.com>
2023-03-07 15:14:04 +0900

    feat: User Config

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,19 +4,28 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { BaseAPIDocumentation } from './config/swagger.document';
 import { PrismaService } from '../prisma/prisma.service';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const port = process.env.SERVER_PORT;
+    const configService = app.get(ConfigService);
 
     const prisma: PrismaService = app.get(PrismaService);
     prisma.enableShutdownHook(app);
 
-    const documentOption = new BaseAPIDocumentation().initializeOptions();
+    const documentOption = new DocumentBuilder()
+    .setTitle('QR Kiosk API')
+    .setDescription('QR Kiosk API backend api')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+
     const document = SwaggerModule.createDocument(app, documentOption);
     SwaggerModule.setup('api-docs', app, document);
 
-    await app.listen(port);
+    await app.listen(configService.get('SERVER_PORT'));
+
     Logger.log(`Application listening on port ${port}`);
 }
 bootstrap();
