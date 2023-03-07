79575ed00d59318a80082e758d75674dc7a4602a
Dark-jin <chch4lee1@naver.com>
2023-03-07 19:38:29 +0900

    feat: login && signup api 제작

---
+++ apps/frontend/src/apis/auth.tsx
@@ -0,0 +1,55 @@
+import { API_URL } from '@src/constants';
+import React from 'react';
+import { NavigateFunction } from 'react-router-dom';
+import { SetterOrUpdater } from 'recoil';
+import axios from 'axios';
+
+const login: string = '/login';
+const signup: string = '/signup';
+
+type loginType = {
+  username: string;
+  password: string;
+};
+type signupType = {
+  username: string;
+  password: string;
+  name: string;
+};
+const headerConfig = {
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Origin': '*',
+};
+const handleError = (error: any) => {
+  if (error.response) {
+    console.log(error.message);
+  }
+};
+const loginAPI = (data: loginType, navigate: NavigateFunction) => {
+  axios
+    .post(API_URL + login, null, {
+      params: data,
+      headers: headerConfig,
+    })
+    .then((response) => {
+      navigate('/store-management');
+    })
+    .catch((error) => {
+      handleError(error);
+    });
+};
+const signupAPI = (data: signupType, navigate: NavigateFunction) => {
+  axios
+    .post(API_URL + signup, null, {
+      params: data,
+      headers: headerConfig,
+    })
+    .then((response) => {
+      window.alert('회원가입이 완료되었습니다. 로그인해주세요.');
+      navigate('/login');
+    })
+    .catch((error) => {
+      handleError(error);
+    });
+};
+export { loginAPI, signupAPI };
--- apps/frontend/src/constants/index.ts
+++ apps/frontend/src/constants/index.ts
@@ -1,1 +1,3 @@
-export {};
+const API_URL = 'http://localhost:8000/api';
+
+export { API_URL };
