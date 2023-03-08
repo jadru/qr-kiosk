7d6a5ce066c5ca0549ed3bcdaf520e96368a3006
Younggun Park <ygpark0922@gmail.com>
2023-03-09 07:27:13 +0900

    feat: payment problem solve && theme fix

--- apps/frontend/src/components/QRprint/QRprint.tsx
+++ apps/frontend/src/components/QRprint/QRprint.tsx
@@ -2,12 +2,15 @@ import React, { forwardRef, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { storeManageState } from '@src/states/atom';
 import QRCode from 'react-qr-code';
+import { Cookies } from 'react-cookie';
 interface Props {
   setTableId: React.Dispatch<React.SetStateAction<string>>;
   tableId: string;
 }
 export const QRprint = forwardRef<HTMLInputElement, Props>(
   ({ tableId }, ref) => {
+    const cookie = new Cookies();
+    const owner_id = cookie.get('owner_id');
     const [storeManage, setStoreManage] = useRecoilState(storeManageState);
 
     return (
@@ -19,7 +22,9 @@ export const QRprint = forwardRef<HTMLInputElement, Props>(
             <h4 className="font-bold">{storeManage.name}</h4>
             <span>테이블 번호 : {tableId}</span>
           </div>
-          <QRCode value={tableId} />
+          <QRCode
+            value={'http://203.241.228.50:9999/' + owner_id + '/' + tableId}
+          />
         </div>
       </div>
     );
