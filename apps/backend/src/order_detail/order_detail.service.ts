25d94c22cd572b9a5740d13b7bf6191249c18540
Hoon9901 <shonn.dev@gmail.com>
2023-03-09 15:25:21 +0900

    feat: schema.prisma add platform

--- apps/backend/src/order_detail/order_detail.service.ts
+++ apps/backend/src/order_detail/order_detail.service.ts
@@ -9,7 +9,6 @@ export class OrderDetailService {
     constructor(private prisma: PrismaService) {}
 
     create(createOrderDetailDto: CreateOrderDetailDto) {
-        
         return this.prisma.order_Detail.create({
             data: {
                 ...createOrderDetailDto,
@@ -17,12 +16,14 @@ export class OrderDetailService {
                     create: createOrderDetailDto.item_orders,
                 },
             },
-            include: { item_orders: true}
+            include: { item_orders: true },
         });
     }
 
     findAll() {
-        return this.prisma.order_Detail.findMany({ include: { item_orders: true }});
+        return this.prisma.order_Detail.findMany({
+            include: { item_orders: true },
+        });
     }
 
     findOne(id: string) {
@@ -33,11 +34,11 @@ export class OrderDetailService {
     }
 
     findByUser(id: string) {
-      return this.prisma.order_Detail.findMany({
-          where: { user_id: id },
-          include: { item_orders: true },
-      });
-  }
+        return this.prisma.order_Detail.findMany({
+            where: { user_id: id },
+            include: { item_orders: true },
+        });
+    }
 
     findByOwner(id: number) {
         return this.prisma.order_Detail.findMany({
@@ -47,33 +48,33 @@ export class OrderDetailService {
     }
 
     async findManyTablesByOwner(id: number) {
-      const orders =  await this.prisma.order_Detail.findMany({
-        where: { owner_id: id },
-        include: { item_orders: true },
-      });
+        const orders = await this.prisma.order_Detail.findMany({
+            where: { owner_id: id },
+            include: { item_orders: true },
+        });
 
-      // orders를 tables_number로 group by 해주는 부분
-      const tables = orders.reduce((acc, cur) => {
-        if (acc[cur.table_number]) {
-          acc[cur.table_number].push(cur);
-        } else {
-          acc[cur.table_number] = [cur];
-        }
-        return acc;
-      }, {});
-      return tables;
-  }
+        // orders를 tables_number로 group by 해주는 부분
+        const tables = orders.reduce((acc, cur) => {
+            if (acc[cur.table_number]) {
+                acc[cur.table_number].push(cur);
+            } else {
+                acc[cur.table_number] = [cur];
+            }
+            return acc;
+        }, {});
+        return tables;
+    }
 
     update(id: string, updateOrderDetailDto: UpdateOrderDetailDto) {
         return this.prisma.order_Detail.update({
             where: { id },
             data: {
-              ...updateOrderDetailDto,
-              item_orders: {
-                  create: updateOrderDetailDto.item_orders,
-              },
-          },
-      });
+                ...updateOrderDetailDto,
+                item_orders: {
+                    create: updateOrderDetailDto.item_orders,
+                },
+            },
+        });
     }
 
     updateOrderStatus(id: string, status: string) {
