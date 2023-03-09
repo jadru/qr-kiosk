fe4567c6acfd47a689ec7f61afc96dec45c852c8
Hoon9901 <shonn.dev@gmail.com>
2023-03-09 15:59:19 +0900

    chore: Image API URL Change

--- apps/backend/src/image/lib/multerOptions.ts
+++ apps/backend/src/image/lib/multerOptions.ts
@@ -2,6 +2,7 @@ import { HttpException } from '@nestjs/common';
 import { existsSync, mkdirSync } from 'fs';
 import { diskStorage } from 'multer';
 import uuidRandom from './uuidRandom';
+import { ConfigService } from '@nestjs/config';
 
 export const multerOptions = {
     fileFilter: (request, file, callback) => {
@@ -35,7 +36,8 @@ export const multerOptions = {
 };
 
 export const createImageURL = (file): string => {
+    const serverAddress: string = "http://203.241.228.50:8000";
     // 파일이 저장되는 경로: /public 폴더
     // 위의 조건에 따라 파일의 경로를 생성해줍니다.
-    return `/public/${file.filename}`;
+    return `${serverAddress}/public/${file.filename}`;
 };
